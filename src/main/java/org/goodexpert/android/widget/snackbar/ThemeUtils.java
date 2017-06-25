0015fdaecfb8110cec0a60a1eed4ec2da49c1f9e
Seongwuk Park <seongwuk.park@gmail.com>
2017-06-26 09:14:15 +1200

    Update snackbar
    - Added the feature that sets the location at which the snackbar should appear on the screen.

---
+++ src/main/java/org/goodexpert/android/widget/snackbar/ThemeUtils.java
@@ -0,0 +1,27 @@
+package org.goodexpert.android.widget.snackbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+/**
+ * Created by Steve on 6/10/2017.
+ */
+
+class ThemeUtils {
+
+    private static final int[] APPCOMPAT_CHECK_ATTRS = {
+            android.support.v7.appcompat.R.attr.colorPrimary
+    };
+
+    static void checkAppCompatTheme(Context context) {
+        TypedArray a = context.obtainStyledAttributes(APPCOMPAT_CHECK_ATTRS);
+        final boolean failed = !a.hasValue(0);
+        if (a != null) {
+            a.recycle();
+        }
+        if (failed) {
+            throw new IllegalArgumentException("You need to use a Theme.AppCompat theme "
+                    + "(or descendant) with the design library.");
+        }
+    }
+}
